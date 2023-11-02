033389091e2871b564bd911ea18452849f9f162b
Titov Nikita <allureshura@mail.ru>
2023-11-03 01:18:14 +0500

    hw1 codestyle fix4

--- src/test/java/edu/hw1/TestArrays.java
+++ src/test/java/edu/hw1/TestArrays.java
@@ -7,7 +7,7 @@ public class TestArrays {
     @Test
     public void testAttachedArrays() {
         Assertions.assertTrue(Arrays.attachedArray(new int[] {2, 2, 3, 1}, new int[] {0, 6}));
-        Assertions.assertTrue(Arrays.attachedArray(new int[] {0, 6}, new int[] {2, 2, 3, 1}));
+        Assertions.assertFalse(Arrays.attachedArray(new int[] {0, 6}, new int[] {2, 2, 3, 1}));
         Assertions.assertTrue(Arrays.attachedArray(new int[] {3, 1}, new int[] {4, 0}));
         Assertions.assertFalse(Arrays.attachedArray(new int[] {9, 9, 8}, new int[] {8, 9}));
         Assertions.assertFalse(Arrays.attachedArray(new int[] {1, 2, 3, 4}, new int[] {2, 3}));
