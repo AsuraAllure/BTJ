da7e1869fbb555bfc57c09e1b0a2cbabb8f26a5f
Titov Nikita <allureshura@mail.ru>
2023-11-06 13:36:51 +0500

    delete useless

--- src/main/java/edu/MazeProject/Main.java
+++ src/main/java/edu/MazeProject/Main.java
@@ -6,16 +6,19 @@ import edu.MazeProject.MazeGenerators.PrimGenerator2D;
 import edu.MazeProject.Representer.StandartMazeRepresenter;
 import edu.MazeProject.Solvers.BFSSolver;
 import edu.MazeProject.Solvers.DFSSolver;
-
+import edu.MazeProject.Structures.RectangleMazeStructures;
 
 public class Main {
     public static void main(String[] s) {
         PrimGenerator2D gen = new PrimGenerator2D(new Configuration(4, 5, 1201534));
-        Maze2D maze = new Maze2D(gen, new StandartMazeRepresenter(System.out));
+
+        RectangleMazeStructures str = gen.generate();
         DFSSolver solv = new DFSSolver();
         BFSSolver solv2 = new BFSSolver();
-        maze.print();
-        maze.printPath(solv.solve(maze.getMazeStructures(), new CordNode(0, 0), new CordNode(3, 4)));
-        maze.printPath(solv2.solve(maze.getMazeStructures(), new CordNode(0, 0), new CordNode(3, 4)));
+        StandartMazeRepresenter rep = new StandartMazeRepresenter(System.out);
+        rep.print(str);
+
+        rep.printPath(str, solv.solve(str, new CordNode(0, 0), new CordNode(3, 4)));
+        rep.printPath(str ,solv2.solve(str, new CordNode(0, 0), new CordNode(3, 4)));
     }
 }
