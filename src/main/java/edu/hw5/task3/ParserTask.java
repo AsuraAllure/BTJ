d12b200f72de82c449808dc3984f2398edf54bb5
Titov Nikita <allureshura@mail.ru>
2023-11-19 03:06:59 +0500

    codestyle

--- src/main/java/edu/hw5/task3/ParserTask.java
+++ src/main/java/edu/hw5/task3/ParserTask.java
@@ -1,6 +1,11 @@
 package edu.hw5.task3;
 
-import edu.hw5.task3.Parsers.*;
+import edu.hw5.task3.Parsers.AgoParser;
+import edu.hw5.task3.Parsers.FailParser;
+import edu.hw5.task3.Parsers.Parser;
+import edu.hw5.task3.Parsers.SlashParser;
+import edu.hw5.task3.Parsers.TireParser;
+import edu.hw5.task3.Parsers.WordParser;
 
 import java.time.Clock;
 
